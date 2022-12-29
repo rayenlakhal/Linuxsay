f929dc04e2b6ee282f0d18b17167028fa0ac9808
rayen lakhal <96541639+rayenlakhal@users.noreply.github.com>
2022-12-29 16:04:45 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string n ;
+	
+	getline(cin, n);
+	cout << endl;
+	cout << endl;
+	
+	cout << "\a" << endl;
+	
+	cout << "  _ _" << endl;
+	cout << " | (_)_ __  _   ___  __" << endl;
+	cout << " | | | '_ \\| | | \\ \\/ /" << endl;
+	cout << " | | | | | | |_| |>  < " << endl;
+	cout << " |_|_|_| |_|\\__,_/_/\\_\\" << endl;
+	
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout <<"        dGGGGMMb     ,\"\"\"\"\"\"\"\"\"\"\"\"\"\"."<< endl;;
+	cout << "       @p~qp~~qMb    " << "| " << n << endl;
+	cout << "       M|@||@) M|   _;..............'" << endl;
+	cout << "       @,----.JM| -'" << endl;
+	cout << "      JS^\\__/  qKL" << endl;
+	cout << "     dZP        qKRb" << endl;
+	cout << "    dZP          qKKb" << endl;
+	cout << "   fZP            SMMb" << endl;
+	cout << "   HZM            MMMM" << endl;
+	cout << "   FqM            MMMM" << endl;
+	cout << " __| \".        |\\dS\"qML" << endl;
+	cout << " |    `.       | `' \\Zq" << endl;
+	cout << "_)      \\.___.,|     .'" << endl;
+	cout << "\\____   )MMMMMM|   .'" << endl;
+	cout << "     `-'       `--'" << endl;
+	return 0;
+}
+
+
